ee45104ffe81d1290877130c5013c4bd57d1658b
Jean Decroocq <jdecroocq.3d@gmail.com>
2025-12-22 23:39:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,39 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(() => console.log('SW enregistré'))
-    .catch(err => console.error('Erreur SW:', err));
-}
+const CACHE_NAME = 'kernel-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/kernel_icon_round_32.png',
+  '/kernel_icon_a_256.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
